6cd4232b543a8ec909141360eb7909c4f4087d18
yuh2150 <huy05.levan@gmail.com>
2025-01-17 11:58:26 +0700

    17_1

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
+import os
 import requests
+from typing import Annotated
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from pydantic import BaseModel, Field 
 from langchain_core.tools import tool
 from langgraph.prebuilt import create_react_agent
+from langgraph.checkpoint.memory import MemorySaver
+from typing_extensions import TypedDict
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+
+from API.booking import BookingAPI
+from API.geoCoding import GeoCodingAPI
+from API.getKey import OAuthClient
+from API.getQuotes import QuotesAPI
+from API.is_Airport import IsAirport
 
 llm = ChatOpenAI(temperature=0, model="gpt-4o-mini")
+jupiterAPI = os.getenv('JUPITER_API')
+quoteAPI = str(jupiterAPI) + "/demand/v1/quotes"
+bookingsAPI  = str(jupiterAPI) + '/demand/v1/bookings'
 
 class BookingCarDetails(BaseModel):
     """Details for the bookings car details"""
@@ -83,7 +100,7 @@ def ask_confirm_info(booking_details: BookingCarDetails):
 def get_booking_details(input_text):
     """ This is function to get information for booking"""
     chain = llm.with_structured_output(BookingCarDetails)
-    response_text = "i want to book a car to 271 Nguyen Van Linh, Da Nang from 460 Tran Dai Nghia, Da Nang at 9 tomorrow "
+    # response_text = "i want to book a car to 271 Nguyen Van Linh, Da Nang from 460 Tran Dai Nghia, Da Nang at 9 tomorrow "
     response = chain.invoke(input_text)
     booking_details = BookingCarDetails(
         name="", number_phone="", pick_up_location="", destination_location="", pick_up_time=""
@@ -105,30 +122,76 @@ def get_booking_details(input_text):
         print(ask_for)
     
     ask_confirm_info(user_details)
+    return user_details
+@tool
+def get_quotes(booking_details : BookingCarDetails):
+    """This function to get quotes for bookings"""
+    quotesAPI = QuotesAPI(os.getenv("JUPITER_API") + "/demand/v1/quotes")
+    geoCodingAPI = GeoCodingAPI()
+    # geoCoding_destination =
+    geoCoding_pickup =  geoCodingAPI.get_geocoding(booking_details.pick_up_location)
+    geoCoding_destination = geoCodingAPI.get_geocoding(booking_details.destination_location)
     
-        
+    # input_datetime = datetime.fromisoformat(pick_up_time)
+    pickup_datetime = "2025-01-18T09:24:10.000Z"
+    
+    pickup_coords = { "latitude": float(geoCoding_pickup['results'][0]['geometry']['location']['lat']),"longitude": float(geoCoding_pickup['results'][0]['geometry']['location']['lng']),}
+    destination_coords = { "latitude": float(geoCoding_destination['results'][0]['geometry']['location']['lat']),"longitude": float(geoCoding_destination['results'][0]['geometry']['location']['lng']),}
+    
+    quotes_data = quotesAPI.get_quotes(pickup_datetime, pickup_coords, destination_coords)
+    
+    return quotes_data
 
-    # # Validate locations
-    # validation_errors = validate_locations(booking_details)
-    # if validation_errors:
-    #     print(f"Validation errors: {validation_errors}")
-    # else:
-    #     print("All details are valid.")
+memory = MemorySaver()
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
 
+graph_builder = StateGraph(State)
+    
+tools = [get_booking_details, get_quotes]   
+llm_with_tools = llm.bind_tools(tools)
+def chatbot(state: State):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+graph_builder.add_node("chatbot", chatbot)
+
+tool_node = ToolNode(tools=[tool])
+graph_builder.add_node("tools", tool_node)
+
+graph_builder.add_conditional_edges(
+    "chatbot",
+    tools_condition,
+)
+# Any time a tool is called, we return to the chatbot to decide the next step
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.add_edge(START, "chatbot")
+
+graph = graph_builder.compile(checkpointer=memory)
 
 if __name__ == "__main__":
-    tools = [get_booking_details]
+    config = {"configurable": {"thread_id": "1"}}
+    
+    graph_builder = StateGraph(State)
+    
     system_prompt = """
     You are a very powerful assistant. Be polite, clear, and understandable.
     If users ask general questions, answer them helpfully. If users want to book a ride, 
     call the 'get_booking_details' function to gather booking information, and pass the user's final input to the function.
-    Remember to keep the user's last input
+    If you have full booking information, ask the user to confirm the booking.
+    If the user confirms, call the 'get_quotes' function to retrieve quotes.
+    
     """
-    agent_executor = create_react_agent(llm, tools = [get_booking_details] , state_modifier=system_prompt)
-    inputs = {"messages": [("user", "my name is Huy,I want to book a car from 271 Nguyen Van Linh to 466 NGuyen van linh da nang at now")]}
-    for s in agent_executor.stream(inputs, stream_mode="values"):
-        message = s["messages"][-1]
-        if isinstance(message, tuple):
-            print(message)
-        else:
-            message.pretty_print()
+    agent_executor = create_react_agent(llm, tools = tools, state_modifier=system_prompt ,graph_modifier=graph)
+    inputs = {"messages": []}  # Khởi tạo danh sách messages
+
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() in ["exit", "quit", "bye"]:
+            print("Exiting the chatbot. Goodbye!")
+            break
+        inputs["messages"].append(("user", user_input))
+        for s in agent_executor.stream(inputs, stream_mode="values"):
+            message = s["messages"][-1]
+            if isinstance(message, tuple):
+                print(f"Assistant: {message[1]}")
+            else:
+                message.pretty_print()
